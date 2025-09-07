4e81d8f0d42017ed3a00628a6f77a0b728d67010
Vaughn-24 <dnance1927@gmail.com>
2025-09-07 08:13:18 -0400

    test(web): configure ts-jest to transpile JSX for web tests

--- apps/web/jest.config.ts
+++ apps/web/jest.config.ts
@@ -5,6 +5,11 @@ const config: Config = {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/__tests__/**/*.spec.ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.jest.json',
+    },
+  },
 };
 
 export default config;
